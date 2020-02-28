5aa277823aa55ee801f2fcf515fd43bc4963244e
richelbilderbeek <richel@richelbilderbeek.nl>
2020-02-28 20:38:11 +0100

    Use of 'pir_save'

--- example_19.R
+++ example_19.R
@@ -2,7 +2,6 @@
 # - Uses 500 nucleotide alignment
 #
 library(pirouette)
-suppressMessages(library(ggplot2))
 
 ################################################################################
 # Constants
@@ -39,6 +38,9 @@ pir_out <- pir_run(
   pir_params = pir_params
 )
 
+################################################################################
+# Save results
+################################################################################
 pir_save(
   phylogeny = phylogeny,
   pir_params = pir_params,
